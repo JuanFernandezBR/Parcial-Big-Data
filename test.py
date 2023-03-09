ddc0b9b787204dee0dba56037b0a397d4e289252
Ubuntu <ubuntu@ip-172-31-49-75.ec2.internal>
2023-03-09 19:19:03 +0000

    arreglando4.0

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-"""
-Your module description
-"""
+import pytest
+from unittest.mock import Mock
+from extraer import extraer()
+import json
+
+def test(mock):
+    
+    mock_info = mock.patch('traer.traer')
+    mock_info.return_value = '{"hits":{"hits":""}}'
+    
+    cantidad = 3
+    resultado = extraer(cantidad)
+    res_dic = json.loads(resultado)
+    
+    assert isinstance(resultado, str)
+    assert resultado[0] == '{'
+    assert "hits" in res_dic.keys()
+    assert len(res_dic["hits"]["hits"]) >= cantidad
