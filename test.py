4f7e0b6193849db13890271322255a1667bd0f3b
JuanFernandezBR <126615220+JuanFernandezBR@users.noreply.github.com>
2023-03-12 19:57:04 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,15 +3,12 @@ from unittest.mock import Mock
 from extraer import extraer
 import json
 
-def test(mock):
-    
-    mock_info = mock.patch('extraer.extraer')
-    mock_info.return_value = '{"hits":{"hits":""}}'
-    
+def test_traer(mocker):
+    mock_traer_datos = mocker.patch('extraer.extraer')
+    mock_traer_datos.return_value = '{"hits":{"hits":""}}'
     cantidad = 3
-    resultado = extraer(cantidad)
+    resultado = traer(cantidad)
     res_dic = json.loads(resultado)
-    
     assert isinstance(resultado, str)
     assert resultado[0] == '{'
     assert "hits" in res_dic.keys()
